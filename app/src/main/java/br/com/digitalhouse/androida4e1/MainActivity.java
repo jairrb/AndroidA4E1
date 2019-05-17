31f72567c1881b6e88cab567fdf0547da10464b6
Jair Ribeiro <jairrb@live.com>
2019-05-17 15:44:00 -0300

    Initial commit

---
+++ app/src/main/java/br/com/digitalhouse/androida4e1/MainActivity.java
@@ -0,0 +1,31 @@
+package br.com.digitalhouse.androida4e1;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Usuário cadastrado com sucesso :)", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+            }
+        });
+    }
+}
